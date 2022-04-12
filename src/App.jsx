6a6c7f256d9d7d901e779dfb3c0fa32553d20a29
Eduardo <el_alienx@hotmail.com>
2022-04-12 13:54:42 +0200

    finish writting the getCollection method

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+// Project files
+import { getCollection } from "./scripts/fireStore";
+
 export default function App() {
+  getCollection("drivers");
+
   return (
     <div className="App">
       <h1>My super fanpage about racing 🏁</h1>
--- src/scripts/fireStore.js
+++ src/scripts/fireStore.js
@@ -2,17 +2,15 @@
 import { collection, getDocs } from "firebase/firestore";
 
 // Project files
-import fireStore from "./firebase";
+import { fireStore } from "./firebase";
 
 // Methods
 export async function getCollection(path) {
   const collectionPath = collection(fireStore, path); // firebase-fe2/firestore/drivers
   const snapshot = await getDocs(collectionPath);
+  const documents = snapshot.docs.map((item) => {
+    return { id: item.id, ...item.data() };
+  });
 
-  console.log("data", snapshot);
+  return documents;
 }
-
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
