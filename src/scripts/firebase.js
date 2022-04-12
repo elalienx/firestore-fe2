5da552d523cf4f2eedd9d626ffcbfb1a99d9b665
Eduardo <el_alienx@hotmail.com>
2022-04-12 13:29:26 +0200

    setup firebase

---
+++ src/scripts/firebase.js
@@ -0,0 +1,14 @@
+// NPM package
+import { initializeApp } from "firebase/app";
+
+// Properties
+const firebaseConfig = {
+  apiKey: "AIzaSyAX8XRB_dKVak22gumWb56h-MxsCBPBDKE",
+  authDomain: "firestore-fe2.firebaseapp.com",
+  projectId: "firestore-fe2",
+  storageBucket: "firestore-fe2.appspot.com",
+  messagingSenderId: "1009972372916",
+  appId: "1:1009972372916:web:45044610cf9ed26edf7ee1",
+};
+
+const app = initializeApp(firebaseConfig);
